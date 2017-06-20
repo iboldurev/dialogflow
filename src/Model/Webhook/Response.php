ed432de4f7aa89e1ae41dba4b1d0f58fe88162f0
Gabriele Maira <gabriele.maira@manifesto.co.uk>
2017-06-20 13:47:30 +0100

    Adding base Webhook\Response methods

--- src/Model/Webhook/Response.php
+++ src/Model/Webhook/Response.php
@@ -3,6 +3,7 @@
 namespace ApiAi\Model\Webhook;
 
 use ApiAi\Model\Base;
+use ApiAi\Model\Context;
 
 /**
  * Class Response.
@@ -12,5 +13,39 @@ use ApiAi\Model\Base;
  * @package ApiAi\Model\Webhook
  */
 class Response extends Base {
+
+  /**
+   * Set response speech.
+   *
+   * @param string $string
+   *   The response speech message.
+   */
+  public function setSpeech($string) {
+    $this->add('speech', $string);
+  }
+
+  /**
+   * Set display text.
+   *
+   * Set the text displayed on the user device screen.
+   *
+   * @param string $string
+   *   The text copy.
+   */
+  public function setDisplayText($string) {
+    $this->add('displayText', $string);
+  }
+
+  /**
+   * Add a context to the response.
+   *
+   * @param \ApiAi\Model\Context $context
+   *   The Context to be added.
+   */
+  public function addContext(Context $context) {
+    $contexts = $this->get('contextOut');
+    $contexts[] = $context;
+    $this->add('contextOut', $contexts);
+  }
 
 }
