a40bdadb33f7ab6f7d00a784289125c12f51df2e
Gabriele Maira <gabriele.maira@manifesto.co.uk>
2017-06-20 09:37:15 +0100

    Adding basic Request/Response webhook models

---
+++ src/Model/Webook/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ApiAi\Model\Webhook;
+
+use ApiAi\Model\QueryResult;
+
+/**
+ * Class Request.
+ *
+ * Provide acess to a webhook Request, which reflect the format of response to
+ * a query.
+ *
+ * @package ApiAi\Model\Webhook
+ */
+class Request extends QueryResult {
+
+}
---
+++ src/Model/Webook/Response.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ApiAi\Model\Webhook;
+
+use ApiAi\Model\Base;
+
+/**
+ * Class Response.
+ *
+ * Data model for a webhook response.
+ *
+ * @package ApiAi\Model\Webhook
+ */
+class Response extends Base {
+
+}
